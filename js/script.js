3c82c12d140f113f4aee7e4c4eb835749c321352
redbrad <shianawati@gmail.com>
2020-01-21 16:27:03 +0700

    menambahkan parallex effect 1. fix background scrolling body 2. scroll effect 3. landing element pd gambar di section portofolio

--- js/script.js
+++ js/script.js
@@ -14,4 +14,34 @@ $('.scroll').on('click',function(){
     scrollTop: elemenTujuan.offset().top - 50
   });
   // e.preventDefault();
+
+});
+
+// parallax
+$(window).scroll(function(){
+
+  let scroll = $(this).scrollTop(); // jarak antar atap body dan window yg sedang dilihat
+
+  // jumbotron
+  $('.jumbotron img').css({
+    'transform' : 'translate(0,'+scroll/10+'%)'
+  });
+  $('.jumbotron h1','.jumbotron p').css({
+    'transform' : 'translate(0,'+scroll/20+'%)'
+  });
+
+  // portofolio
+  if(scroll>$('.portofolio').offset().top - 150) {
+    $('.thumb').each(function(i) {
+      setTimeout(function() {
+        $('.thumb').eq(i).addClass('muncul');
+      }, 200*(i+1));
+    });
+  }
+
+  if(scroll>$('.about').offset().top - 150) {
+    $('.kiri').addClass('muncul');
+    $('.kanan').addClass('muncul');
+  }
+
 });
